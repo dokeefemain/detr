805800e3206325fc4f2064de4360a4d3688b2198
devinokeefe <dokeefe3@student.gsu.edu>
2022-10-25 17:18:40 -0400

    updates

--- main.py
+++ main.py
@@ -102,6 +102,7 @@ def get_args_parser():
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--pretrained', default=False)
     return parser
 
 
--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from .detr import build
+from .detr import build as build1
+from .detr_pretrained import build
 
 
 def build_model(args):
-    return build(args)
+    if args.pretrained is None:
+        return build1(args)
+    else:
+        return build(args)
